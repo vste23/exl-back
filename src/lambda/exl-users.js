483aa47c9b832183af326a7b696abbacfe620347
Valentyn Stets <valik.stets@gmail.com>
2020-10-26 23:07:44 +0200

    Addec cors headers

--- src/lambda/exl-users.js
+++ src/lambda/exl-users.js
@@ -1,6 +1,13 @@
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
+
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
+    headers,
     body: JSON.stringify({users: [167901]})
   });
 };
