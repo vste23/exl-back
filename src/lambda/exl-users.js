0ca13c984540c722384906ce768be80d724db773
Valentyn Stets <valik.stets@gmail.com>
2020-10-28 12:45:50 +0200

    Updated list of users

--- src/lambda/exl-users.js
+++ src/lambda/exl-users.js
@@ -8,6 +8,6 @@ exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
     headers,
-    body: JSON.stringify({users: [167901]})
+    body: JSON.stringify({users: [167901, 136049]})
   });
 };
